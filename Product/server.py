543b4732fd4cf4078e10875a138c0b4553c9f9b7
SEAN0905 <seanxinyu2@gmail.com>
2019-07-12 20:26:44 +0800

    some updates

--- Product/server.py
+++ Product/server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
+import shutil
+# shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
 
 def open_DB(db):
     connection = sqlite3.connect(db)
@@ -10,6 +13,7 @@ app = Flask("__name__")
 
 @app.route("/")
 def root():
+    # TODO: UI to be updated
     return render_template("camera.html")
 
 @app.route("/addUser", methods=["POST"])
@@ -25,6 +29,20 @@ def add_user():
     con.close()
     # TODO: supposed to render a certain page 
     return render_template("products.html")
+
+# triggered after clicking the snap button in the camera page
+@app.route("/classify_page", methods=["POST"])
+def classify():
+    # TODO: shift the image to specific folder
+    # TODO: run the deep learning model 
+    # TODO: make prediction of the classification
+    # TODO: play animation of the classification process
+    # TODO: trigger reward system when mistakes were spotted
+    # TODO: move the image to trash folder
+    # TODO: display ending page
+    return render_template("")
+
+
 
 if __name__ == "__main__":
     app.run()
