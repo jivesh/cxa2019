d534ff56183355663cebf9912c37192194c405a7
SEAN0905 <seanxinyu2@gmail.com>
2019-07-12 19:39:59 +0800

    back-end folder

---
+++ Product/create_db.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+con = sqlite3.connect("user.db")
+
+try:
+    con.execute("CREATE TABLE User " + 
+                    " (Name TEXT PRIMARY KEY, Points INTEGER )")
+except Exception as err:
+    print(str(err))
+
+while True:
+    name = input("Enter User name: \n")
+    if name == "":
+        break
+    points = input("Enter current points of the User:\n")
+    try:
+        con.execute("INSERT INTO User(Name, Points) VALUES(?, ?)", (name, points))
+    except Exception as err:
+        print(str(err), "Try another id")
+        continue
+    con.commit()
+con.close()
