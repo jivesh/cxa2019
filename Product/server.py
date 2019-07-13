4c2d55fc7029eb517208aab0a6c528077d71c21a
SEAN0905 <seanxinyu2@gmail.com>
2019-07-13 09:36:26 +0800

    basic user login system

--- Product/server.py
+++ Product/server.py
@@ -5,31 +5,58 @@ import shutil
 import time
 import random
 
+
 def open_DB(db):
     connection = sqlite3.connect(db)
     connection.row_factory = sqlite3.Row
     return connection
 
+
 app = Flask("__name__")
+
 
 @app.route("/")
 def root():
+    return render_template("log_in.html")
+
+
+# # TODO
+# @app.route("/addUser", methods=["POST"])
+# def add_user():
+#     # the function is to create a new user
+#     con = open_DB('user.db')
+#     try:
+#         user_name = request.form['name']
+#         con.execute("INSERT INTO User (Name, Points) VALUES(?, ?)", (user_name, 0))
+#         con.commit()
+#     except:
+#         print("Database error")
+#     con.close()
+#     # TODO: supposed to render a certain page
+#     return render_template("products.html")
+
+
+@app.route("/main_page", methods=["POST"])
+def main_page():
+    con = open_DB('user.db')
+    input_name = request.form["user_name"]
+    try:
+        record = con.execute("SELECT Name, Points FROM User WHERE Name = \"" +input_name+"\"")
+        np_dict = record.fetchall()
+        for line in np_dict:
+            point = line["Points"]
+    except:
+        point = 0
+        con.execute("INSERT INTO User (Name, Points) VALUES(?, ?)",
+                    (input_name, point))
+        con.commit()
+    return render_template("main_page.html", point=point)
+
+
+@app.route("/throw_rubbish", methods=["POST"])
+def throw_rubbish():
     return render_template("camera.html")
 
-# TODO
-@app.route("/addUser", methods=["POST"])
-def add_user():
-    # the function is to create a new user
-    con = open_DB('user.db')
-    try:
-        user_name = request.form['name']
-        con.execute("INSERT INTO User (Name, Points) VALUES(?, ?)", (user_name, 0))
-        con.commit()
-    except:
-        print("Database error")
-    con.close()
-    # TODO: supposed to render a certain page 
-    return render_template("products.html")
 
 # triggered after clicking the snap button in the camera page
 @app.route("/classify", methods=["GET"])
@@ -43,7 +70,14 @@ def classify():
     for line in result:
         result_str = line
     # update the count of each type of trash
-    labels = {'cardboard':0, 'glass':1, 'metal':2, 'paper':3, 'plastic':4, 'trash':5}
+    labels = {
+        'cardboard': 0,
+        'glass': 1,
+        'metal': 2,
+        'paper': 3,
+        'plastic': 4,
+        'trash': 5
+    }
     index = labels[result_str]
     handle = open("counter.txt", "r")
     for line in handle:
@@ -57,11 +91,14 @@ def classify():
             handle.write(", ")
     handle.close()
     # remove the image
-    os.remove("/Users/ue/Downloads/CXA2019/cxa2019/Product/static/images/downloaded_images/picture/trash.png")
+    os.remove(
+        "/Users/ue/Downloads/CXA2019/cxa2019/Product/static/images/downloaded_images/picture/trash.png"
+    )
     # play the anima for each type of trash
     final_url = result_str + ".html"
     return render_template(final_url)
-    
+
+
 @app.route("/reward", methods=["GET"])
 def reward():
     # TODO: trigger reward system when mistakes were spotted
@@ -69,7 +106,6 @@ def reward():
     # TODO: display ending page
     # return render_template("")
     return "Hey"
-
 
 
 if __name__ == "__main__":
