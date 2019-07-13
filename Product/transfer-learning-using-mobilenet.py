a468a51fbd1bb6adf7e7a3ce1e787e1ae5dacfba
SEAN0905 <seanxinyu2@gmail.com>
2019-07-13 16:00:42 +0800

    some updates during last testing

--- Product/transfer-learning-using-mobilenet.py
+++ Product/transfer-learning-using-mobilenet.py
@@ -1,204 +1,204 @@
-#!/usr/bin/env python
-# coding: utf-8
+# #!/usr/bin/env python
+# # coding: utf-8
 
-# In[1]:
-from __future__ import absolute_import, division, print_function
+# # In[1]:
+# from __future__ import absolute_import, division, print_function
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in
+# # This Python 3 environment comes with many helpful analytics libraries installed
+# # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# # For example, here's several helpful packages to load in
 
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+# import numpy as np  # linear algebra
+# import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+# # Input data files are available in the "../input/" directory.
+# # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-import os
+# import os
 
-# In[2]:
+# # In[2]:
 
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+# import os
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-# In[3]:
+# # In[3]:
 
-import zipfile
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import glob, os, random
+# import zipfile
+# import tensorflow as tf
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# import glob, os, random
 
-# In[4]:
+# # In[4]:
 
-import numpy as np
-from keras.preprocessing import image
-import matplotlib.pyplot as plt
-base_path = 'Garbage classification'
-img_list = glob.glob(os.path.join(base_path, '*/*.jpg'))
+# import numpy as np
+# from keras.preprocessing import image
+# import matplotlib.pyplot as plt
+# base_path = 'Garbage classification'
+# img_list = glob.glob(os.path.join(base_path, '*/*.jpg'))
 
-for i, img_path in enumerate(random.sample(img_list, 6)):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img = image.img_to_array(img, dtype=np.uint8)
+# for i, img_path in enumerate(random.sample(img_list, 6)):
+#     img = image.load_img(img_path, target_size=(224, 224))
+#     img = image.img_to_array(img, dtype=np.uint8)
 
-    plt.subplot(2, 3, i + 1)
-    plt.imshow(img.squeeze())
+#     plt.subplot(2, 3, i + 1)
+#     plt.imshow(img.squeeze())
 
-# In[5]:
+# # In[5]:
 
-train_datagen = ImageDataGenerator(rescale=1. / 255,
-                                   shear_range=0.1,
-                                   zoom_range=0.1,
-                                   width_shift_range=0.1,
-                                   height_shift_range=0.1,
-                                   horizontal_flip=True,
-                                   vertical_flip=True,
-                                   validation_split=0.1)
+# train_datagen = ImageDataGenerator(rescale=1. / 255,
+#                                    shear_range=0.1,
+#                                    zoom_range=0.1,
+#                                    width_shift_range=0.1,
+#                                    height_shift_range=0.1,
+#                                    horizontal_flip=True,
+#                                    vertical_flip=True,
+#                                    validation_split=0.1)
 
-test_datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.1)
+# test_datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.1)
 
-train_generator = train_datagen.flow_from_directory(base_path,
-                                                    target_size=(224, 224),
-                                                    batch_size=16,
-                                                    class_mode='categorical',
-                                                    subset='training',
-                                                    seed=0)
+# train_generator = train_datagen.flow_from_directory(base_path,
+#                                                     target_size=(224, 224),
+#                                                     batch_size=16,
+#                                                     class_mode='categorical',
+#                                                     subset='training',
+#                                                     seed=0)
 
-validation_generator = test_datagen.flow_from_directory(
-    base_path,
-    target_size=(224, 224),
-    batch_size=16,
-    class_mode='categorical',
-    subset='validation',
-    seed=0)
+# validation_generator = test_datagen.flow_from_directory(
+#     base_path,
+#     target_size=(224, 224),
+#     batch_size=16,
+#     class_mode='categorical',
+#     subset='validation',
+#     seed=0)
 
-# In[6]:
+# # In[6]:
 
-IMG_SHAPE = (224, 224, 3)
-base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
-                                               include_top=False,
-                                               weights='imagenet')
+# IMG_SHAPE = (224, 224, 3)
+# base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+#                                                include_top=False,
+#                                                weights='imagenet')
 
-# In[7]:
+# # In[7]:
 
-base_model.trainable = False
+# base_model.trainable = False
 
-# In[8]:
+# # In[8]:
 
-from tensorflow import keras
-model = tf.keras.Sequential([
-    base_model,
-    keras.layers.GlobalAveragePooling2D(),
-    keras.layers.Dense(6, activation='sigmoid')
-])
+# from tensorflow import keras
+# model = tf.keras.Sequential([
+#     base_model,
+#     keras.layers.GlobalAveragePooling2D(),
+#     keras.layers.Dense(6, activation='sigmoid')
+# ])
 
-# In[9]:
+# # In[9]:
 
-# model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001),
-#               loss='categorical_crossentropy',
+# # model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001),
+# #               loss='categorical_crossentropy',
+# #               metrics=['accuracy'])
+
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+# # In[10]:
+
+# batch_size = 32
+# epochs = 10
+# steps_per_epoch = train_generator.n // batch_size
+# validation_steps = validation_generator.n // batch_size
+
+# history = model.fit_generator(train_generator,
+#                               steps_per_epoch=steps_per_epoch,
+#                               epochs=epochs,
+#                               workers=4,
+#                               validation_data=validation_generator,
+#                               validation_steps=validation_steps)
+
+# # In[11]:
+
+# acc = history.history['acc']
+# val_acc = history.history['val_acc']
+
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([min(plt.ylim()), 1])
+# plt.title('Training and Validation Accuracy')
+
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.ylabel('Cross Entropy')
+# plt.ylim([0, max(plt.ylim())])
+# plt.title('Training and Validation Loss')
+# plt.show()
+
+# # ## Fine Tuning
+
+# # In[12]:
+
+# base_model.trainable = True
+# # Let's take a look to see how many layers are in the base model
+# print("Number of layers in the base model: ", len(base_model.layers))
+
+# # Fine tune from this layer onwards
+# fine_tune_at = 100
+
+# # Freeze all the layers before the `fine_tune_at` layer
+# for layer in base_model.layers[:fine_tune_at]:
+#     layer.trainable = False
+
+# # In[13]:
+
+# model.compile(loss='binary_crossentropy',
+#               optimizer=tf.keras.optimizers.RMSprop(lr=2e-5),
 #               metrics=['accuracy'])
+# model.summary()
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+# # In[14]:
 
-# In[10]:
+# history_fine = model.fit_generator(train_generator,
+#                                    steps_per_epoch=steps_per_epoch,
+#                                    epochs=epochs,
+#                                    workers=4,
+#                                    validation_data=validation_generator,
+#                                    validation_steps=validation_steps)
 
-batch_size = 32
-epochs = 10
-steps_per_epoch = train_generator.n // batch_size
-validation_steps = validation_generator.n // batch_size
+# # In[15]:
 
-history = model.fit_generator(train_generator,
-                              steps_per_epoch=steps_per_epoch,
-                              epochs=epochs,
-                              workers=4,
-                              validation_data=validation_generator,
-                              validation_steps=validation_steps)
+# acc += history_fine.history['acc']
+# val_acc += history_fine.history['val_acc']
 
-# In[11]:
+# loss += history_fine.history['loss']
+# val_loss += history_fine.history['val_loss']
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+# # In[16]:
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.ylim([0.9, 1])
+# plt.plot([epochs - 1, epochs - 1], plt.ylim(), label='Start Fine Tuning')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
 
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.ylabel('Accuracy')
-plt.ylim([min(plt.ylim()), 1])
-plt.title('Training and Validation Accuracy')
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.ylim([0, 0.2])
+# plt.plot([epochs - 1, epochs - 1], plt.ylim(), label='Start Fine Tuning')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
 
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.ylabel('Cross Entropy')
-plt.ylim([0, max(plt.ylim())])
-plt.title('Training and Validation Loss')
-plt.show()
-
-# ## Fine Tuning
-
-# In[12]:
-
-base_model.trainable = True
-# Let's take a look to see how many layers are in the base model
-print("Number of layers in the base model: ", len(base_model.layers))
-
-# Fine tune from this layer onwards
-fine_tune_at = 100
-
-# Freeze all the layers before the `fine_tune_at` layer
-for layer in base_model.layers[:fine_tune_at]:
-    layer.trainable = False
-
-# In[13]:
-
-model.compile(loss='binary_crossentropy',
-              optimizer=tf.keras.optimizers.RMSprop(lr=2e-5),
-              metrics=['accuracy'])
-model.summary()
-
-# In[14]:
-
-history_fine = model.fit_generator(train_generator,
-                                   steps_per_epoch=steps_per_epoch,
-                                   epochs=epochs,
-                                   workers=4,
-                                   validation_data=validation_generator,
-                                   validation_steps=validation_steps)
-
-# In[15]:
-
-acc += history_fine.history['acc']
-val_acc += history_fine.history['val_acc']
-
-loss += history_fine.history['loss']
-val_loss += history_fine.history['val_loss']
-
-# In[16]:
-
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.ylim([0.9, 1])
-plt.plot([epochs - 1, epochs - 1], plt.ylim(), label='Start Fine Tuning')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.ylim([0, 0.2])
-plt.plot([epochs - 1, epochs - 1], plt.ylim(), label='Start Fine Tuning')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-
-# In[17]:
+# # In[17]:
